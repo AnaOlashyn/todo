e35b97caf7e54d6fa691500b7dee901a6382c361
AnaOlashyn <anaolashyn@gmail.com>
2020-11-21 16:28:29 +0100

    init

---
+++ todo/src/app/input-button-unit/input-button-unit.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-input-button-unit',
+  templateUrl: './input-button-unit.component.html',
+  styleUrls: ['./input-button-unit.component.scss']
+})
+export class InputButtonUnitComponent implements OnInit {
+  title = 'Hello World';
+  @Output() submit: EventEmitter<string> = new EventEmitter();
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+  changeTitle(newTitle: string) {
+    this.submit.emit(newTitle);
+  }
+}
